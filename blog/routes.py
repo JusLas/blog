556d45c672b239e977871272d593152453a55a4d
JusLas <justyna.laszuk@gmail.com>
2021-12-18 11:59:52 +0100

    Zaimplementowano mechanizm logowania

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,41 @@
-from flask import render_template, request, flash
+from flask import render_template, request, session, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
 
 
 @app.route("/")
@@ -13,11 +47,13 @@ def index():
 
 
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
 def create_entry():
     return create_or_update()
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
     entry = Entry.query.filter_by(id=entry_id).first_or_404()
     return create_or_update(entry)
