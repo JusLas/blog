d93cee888e59def98d114e710859d31f0616bfa8
JusLas <justyna.laszuk@gmail.com>
2021-12-18 09:16:09 +0100

    Zaimplementowano formularz dodawania nowego wpisu

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-   SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
-   SQLALCHEMY_DATABASE_URI = (
-           os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
-   )
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY") or "56d0725a8b9934d4a8973e33534aa981"
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') or
+        'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
